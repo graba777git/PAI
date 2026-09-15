1e8268680fa8a27224a3b1d8f8177634f4864f01
Michał Grabowski <mgrabowski@technikumlodz.pl>
2026-09-15 13:20:52 +0200

    Zadania php

---
+++ Zadanie 4/index.php
@@ -0,0 +1,73 @@
+<!-- <?php
+
+$liczba = 8;
+if ($liczba % 2 == 0){
+    echo"liczba jest parzysta";
+}else{
+    echo"liczba jest nieparzysta";
+}
+
+
+?> -->
+
+
+<!-- <?php
+
+$liczba1 = 10;
+$liczba2 = 3;
+if ($liczba1 % $liczba2 == 0){
+    echo"pierwsza liczba jest podzielna przez druga";
+}else{
+    echo"pierwsza liczba nie jest podzielna przez druga";
+}
+
+
+?> -->
+
+
+<!-- <?php
+
+$liczba1 = 18;
+
+if (($liczba >= 1 && $liczba <= 10 or $liczba >= 17 && $liczba <= 21)){
+    echo"liczba nalezy do przedzialu";
+}else{
+    echo"liczba nie lezy do przedzialu";
+}
+
+//Zad 4
+
+// $a = 5;
+
+// if($a > 0){
+//     echo "Wieksza od zera";
+// }else {
+//     echo "Mniejsza od zera";
+// }
+
+
+//Zad 5
+
+$a = 15;
+
+if($a < 11){
+    echo "Dziecko";
+} elseif($a >= 11 && $a <= 17){
+    echo "nastolatek";
+} elseif($a > 18){
+    echo "dorosły";
+}
+
+
+
+
+
+
+
+?> -->
+
+
+
+
+
+
